53182413fcfd5068fad6a82578f2ecc3a32ba7f9
Oleg Smirnov <smirnovob1417@mail.ru>
2022-12-19 19:27:11 +0300

    Final

--- DZ/Program.cs
+++ DZ/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿string[] Array = { "hello", "2", ":-)", "g" };
+NewArray(Array);
+
+string[] NewArray(string[] Array)
+{
+    string[] answer = new string[Array.Length];
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i].Length <= 3)
+        {
+            answer[i] = Array[i];
+        }
+    }
+    Console.Write("[");
+    Console.Write(string.Join(" ", answer));
+    Console.Write("]");
+    return answer;
+}
